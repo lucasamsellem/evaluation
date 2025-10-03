2bd7f7202c975b198565d23497d806f40a3e4cb5
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-10-03 15:42:27 +0200

    feat: added validations on forms

--- controllers/user.js
+++ controllers/user.js
@@ -1,144 +1,146 @@
-import User from "../db/models/User.js";
-import crypto from "crypto";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import User from '../db/models/User.js';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const { JWT_SECRET } = process.env;
 
 export function getLoginForm(req, res) {
-	res.render("login");
+  res.render('login');
 }
 
 export function getRegisterForm(req, res) {
-	res.render("register");
+  res.render('register');
 }
 
 export async function postLoginForm(req, res) {
-	if (!req.body.name || !req.body.password) {
-		req.session.message = {
-			type: "error",
-			message: "Merci de remplir tout les champs du formulaire",
-		};
+  if (!req.body.name || !req.body.password) {
+    req.session.message = {
+      type: 'error',
+      message: 'Merci de remplir tout les champs du formulaire',
+    };
 
-		res.redirect("/login");
-		return;
-	}
+    res.redirect('/login');
+    return;
+  }
 
-<<<<<<< HEAD
-	const { name, password } = req.body;
-	console.log("here");
-=======
   const { name, password } = req.body;
->>>>>>> 9e205c1085ce3d5d116ee44e44b5e732122445cb
 
-	if (name.trim() === "" || password.trim() === "") {
-		req.session.message = {
-			type: "error",
-			message: "Merci de remplir tout les champs du formulaire",
-		};
+  if (name.trim() === '' || password.trim() === '') {
+    req.session.message = {
+      type: 'error',
+      message: 'Merci de remplir tout les champs du formulaire',
+    };
 
-		res.redirect("/login");
-		return;
-	}
+    res.redirect('/login');
+    return;
+  }
 
-	const user = await User.findOne({ email: name });
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+  if (!passwordRegex.test(password)) {
+    req.session.message = {
+      type: 'error',
+      message:
+        'Le mot de passe doit comporter au moins 8 caractères, 1 majuscule, 1 chiffre et 1 caractère spécial',
+    };
+    return res.redirect('/login');
+  }
 
-	if (!user) {
-		req.session.message = { type: "error", message: "Identifiant inconnu" };
-		res.redirect("/login");
-	}
+  const user = await User.findOne({ email: name });
 
-	const hashedInput = crypto
-		.createHmac("sha256", process.env.PASSWORD_SECRET)
-		.update(password)
-		.digest("hex");
+  if (!user) {
+    req.session.message = { type: 'error', message: 'Identifiant inconnu' };
+    res.redirect('/login');
+  }
 
-<<<<<<< HEAD
-	if (user.password !== hashedInput) {
-		console.log("ERREUR CRYPTO");
-		req.session.toast = { type: "error", message: "Identifiants inconnu" };
-		res.redirect("/login");
-		return;
-	}
-=======
+  const hashedInput = crypto
+    .createHmac('sha256', process.env.PASSWORD_SECRET)
+    .update(password)
+    .digest('hex');
+
   if (user.password !== hashedInput) {
     req.session.toast = { type: 'error', message: 'Identifiants inconnu' };
     res.redirect('/login');
     return;
   }
->>>>>>> 9e205c1085ce3d5d116ee44e44b5e732122445cb
 
-	const token = jwt.sign({ id: user._id }, JWT_SECRET, {
-		algorithm: "HS256",
-	});
+  const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+    algorithm: 'HS256',
+  });
 
-	if (user.role === "admin") {
-		req.session.isAdmin = true;
-	}
-	req.session.token = token;
-	req.session.isConnected = true;
-	req.session.message = { type: "success", message: "Connécté avec succès." };
-	res.locals.isConnected = true;
+  if (user.role === 'admin') {
+    req.session.isAdmin = true;
+  }
+  req.session.token = token;
+  req.session.isConnected = true;
+  req.session.message = { type: 'success', message: 'Connécté avec succès.' };
+  res.locals.isConnected = true;
 
-	res.redirect("/");
+  res.redirect('/');
 }
 
 export async function postRegisterForm(req, res) {
-	const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password, passwordConfirmation } = req.body;
 
-	if (!firstName || !lastName || !email || !password) {
-		req.session.message = {
-			type: "error",
-			message: "Merci de remplir tous les champs du formulaire",
-		};
-		return res.redirect("/register");
-	}
+  if (!firstName || !lastName || !email || !password || !passwordConfirmation) {
+    req.session.message = {
+      type: 'error',
+      message: 'Merci de remplir tous les champs du formulaire',
+    };
+    return res.redirect('/register');
+  }
 
-	const hashedPassword = crypto
-		.createHmac("sha256", process.env.PASSWORD_SECRET)
-		.update(password)
-		.digest("hex");
+  if (password !== passwordConfirmation) {
+    req.session.message = {
+      type: 'error',
+      message: 'Les mots de passe ne correspondent pas',
+    };
+    return res.redirect('/register');
+  }
 
-	const newUser = new User({
-		firstName,
-		lastName,
-		email,
-		password: hashedPassword,
-	});
+  const existingUser = await User.findOne({ email });
 
-	await newUser.save();
+  if (existingUser) {
+    req.session.message = {
+      type: 'error',
+      message: 'Cet utilisateur existe deja',
+    };
+    return res.redirect('/register');
+  }
 
-	req.session.message = {
-		type: "success",
-		message: "Votre compte a été créé avec succès !",
-	};
+  const hashedPassword = crypto
+    .createHmac('sha256', process.env.PASSWORD_SECRET)
+    .update(password)
+    .digest('hex');
 
-	res.redirect("/login");
+  const newUser = new User({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  req.session.message = {
+    type: 'success',
+    message: 'Votre compte a été créé avec succès !',
+  };
+
+  res.redirect('/login');
 }
 
 export function logout(req, res) {
-<<<<<<< HEAD
-	req.session.destroy((err) => {
-		if (err) {
-			console.error("Erreur de déconnexion :", err);
-			return res.redirect("/"); // ou une page d’erreur
-		}
-		res.locals.isConnected = false;
-		res.locals.isAdmin = false;
-		res.clearCookie("connect.sid"); // optionnel mais propre
-		res.redirect("/");
-	});
-=======
   req.session.destroy((err) => {
     if (err) {
       console.error('Erreur de déconnexion :', err);
       return res.redirect('/'); // ou une page d’erreur
     }
-
+    res.locals.isConnected = false;
+    res.locals.isAdmin = false;
     res.clearCookie('connect.sid'); // optionnel mais propre
     res.redirect('/login');
   });
->>>>>>> 9e205c1085ce3d5d116ee44e44b5e732122445cb
 }
